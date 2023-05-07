06194be18043331c6d90fb08815601db534949b5
Smertig <akaraevz@mail.ru>
2023-05-07 19:31:38 +0300

    Try to fix workflow in case of concurrent reports

--- .github/save_stats.py
+++ .github/save_stats.py
@@ -1,35 +1,37 @@
 import json
 import os
 import sys
+import pathlib
 
 
 def main() -> int:
-    assert len(sys.argv) == 3, f"{sys.argv[0]} report_path repo_path"
-    _, report_path, repo_path = sys.argv
+    assert len(sys.argv) == 3, f"{sys.argv[0]} reports_dir repo_path"
+    _, reports_dir, repo_path = sys.argv
 
-    with open(report_path) as f_report:
-        report = json.load(f_report)
-        for project_name, toolchains in report.items():
-            branch = None
-            if ':' in project_name:
-                project_name, branch = project_name.split(':', 1)
+    for report_path in pathlib.Path(reports_dir).glob('*-report/report.json'):
+        with open(report_path) as f_report:
+            report = json.load(f_report)
+            for project_name, toolchains in report.items():
+                branch = None
+                if ':' in project_name:
+                    project_name, branch = project_name.split(':', 1)
 
-            for toolchain_name, project_report in toolchains.items():
-                cache_path = f'{repo_path}/{project_name}/{branch or "stable"}_{toolchain_name}.json'
-                os.makedirs(os.path.dirname(cache_path), exist_ok=True)
+                for toolchain_name, project_report in toolchains.items():
+                    cache_path = f'{repo_path}/{project_name}/{branch or "stable"}_{toolchain_name}.json'
+                    os.makedirs(os.path.dirname(cache_path), exist_ok=True)
 
-                # load existing cache
-                cache = {'v1': []}
-                if os.path.exists(cache_path):
-                    with open(cache_path) as f_cache:
-                        cache = json.load(f_cache)
+                    # load existing cache
+                    cache = {'v1': []}
+                    if os.path.exists(cache_path):
+                        with open(cache_path) as f_cache:
+                            cache = json.load(f_cache)
 
-                # update
-                cache['v1'].append(project_report)
+                    # update
+                    cache['v1'].append(project_report)
 
-                # dump cache
-                with open(cache_path, 'w') as f_cache:
-                    json.dump(cache, f_cache, indent=4)
+                    # dump cache
+                    with open(cache_path, 'w') as f_cache:
+                        json.dump(cache, f_cache, indent=4)
 
     return 0
 
