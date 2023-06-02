0f51a3aa9d2e9d0648ec0a1c164af9e91f582d0d
Alexander Karaev <Alexander.Karaev@jetbrains.com>
2023-06-02 17:30:53 +0300

    Workaround OOM for 'LLVM' project

--- proj-config/scripts/fix-llvm.py
+++ proj-config/scripts/fix-llvm.py
@@ -4,3 +4,8 @@ import utils
 utils.update_file('llvm/include/llvm/Support/TrailingObjects.h',
                   lambda content: content.replace(b'class TrailingObjects : private trailing_objects_internal',
                                                   b'class TrailingObjects : public trailing_objects_internal'))
+
+# Workaround for RSCPP-34288 STOFL on recursive object
+utils.update_file('clang/lib/Format/Format.cpp',
+                  lambda content: content.replace(b'static const ParseErrorCategory C{}',
+                                                  b'static /*const*/ ParseErrorCategory C{}'))
