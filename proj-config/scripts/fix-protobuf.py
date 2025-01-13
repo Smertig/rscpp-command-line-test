970e18c4793ef6f682a948aa49847d88d8aaac19
Smertig <akaraevz@mail.ru>
2025-01-13 19:18:53 +0200

    Update some known errors [ci-all]

--- proj-config/scripts/fix-protobuf.py
+++ proj-config/scripts/fix-protobuf.py
@@ -10,5 +10,8 @@ utils.update_file('src/google/protobuf/port_def.inc',
                   lambda content: content.replace(b'#ifdef PROTOBUF_EXPORT',
                                                   b'#define PROTOBUF_FIELD_OFFSET(TYPE, FIELD) 0\n#ifdef PROTOBUF_EXPORT'))
 
-utils.update_file('third_party/abseil-cpp/absl/base/config.h',
-                  lambda content: content.replace(b'#elif defined(__clang__) && (__clang_major__ >= 15)', b'#else'))
+try:
+    utils.update_file('third_party/abseil-cpp/absl/base/config.h',
+                      lambda content: content.replace(b'#elif defined(__clang__) && (__clang_major__ >= 15)', b'#else'))
+except Exception:
+    pass
